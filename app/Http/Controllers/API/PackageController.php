465883a091c11554443017501d57f5dff8fc23df
fiaufar <fiaufar@gmail.com>
2022-02-06 21:06:44 +0700

    Create Package Controller

---
+++ app/Http/Controllers/API/PackageController.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Package;
+use Illuminate\Http\Request;
+
+class PackageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $data = Package::all();
+
+        return response()->json($data, 200);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // dd($request->all());
+        $data = Package::create($request->all());
+
+        return response()->json($data, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $packaga = Package::findOrFail($id);
+        $packaga->update($request->all());
+
+        return response()->json($packaga, 201);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function updatePartial(Request $request, $id)
+    {
+        $packaga = Package::findOrFail($id);
+        foreach ($request->all() as $key => $value) {
+            $packaga[$key] = $value;
+        }
+
+        $packaga->save();
+
+        return response()->json($packaga, 201);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $packaga = Package::findOrFail($id);
+        $packaga->delete();
+
+        return response()->json('Success', 201);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/package', [ App\Http\Controllers\API\PackageController::class, 'index']);
+Route::post('/package', [App\Http\Controllers\API\PackageController::class, 'store']);
+Route::put( '/package/{id}', [App\Http\Controllers\API\PackageController::class, 'update']);
+Route::patch('/package/{id}', [App\Http\Controllers\API\PackageController::class, 'updatePartial']);
+Route::delete('/package/{id}', [App\Http\Controllers\API\PackageController::class, 'destroy']);
